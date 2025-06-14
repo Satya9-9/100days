81e528370ed6023018ec093a0bdb7d9b567bafc1
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2025-06-15 00:19:57 +0530

    cesar cipher cryptography

--- day7.py
+++ day7.py
@@ -2,33 +2,45 @@ from pyfiglet import Figlet
 
 f = Figlet(font='slant')  # try fonts like 'block', 'bubble', 'banner'
 print(f.renderText('CIPHER'))
-
+encode_or_decode = input("encode or decode: ")
 alphabets = [chr(i) for i in range(97, 123)]
-
-message = input("enter a message: ").lower()
+message = input("Write a message: ").lower()
 shift = int(input("enter shift: "))
-newmessage = ""
-newmessage2 = ""
-for index, letter in enumerate(message):
-    print(index, letter)
-    if letter in alphabets:
-        newindex = (alphabets.index(letter) + shift) % 26
-
-        newmessage += alphabets[newindex]
-
-    elif letter == " ":
-        newmessage += " "
-
-for index, letter2 in enumerate(newmessage):
-    print(index, letter2)
-    if letter2 in alphabets:
-        newindex2 = (alphabets.index(letter2) - shift) % 26 # here negative indexing works
 
 
-        newmessage2 += alphabets[newindex2]
+def encoder(orginal_text, shift_amt, encode_or_decode):
+    message = orginal_text.lower()
+    shift = shift_amt
+    cipher_text = ""
 
-    elif letter2 == " ":
-        newmessage2 += " "
+    for letter in message:
+        if letter in alphabets:
+            newindex = (alphabets.index(letter) + shift) % 26
+            cipher_text += alphabets[newindex]
 
-print(newmessage)
-print(newmessage2)
+        else:
+            cipher_text += letter
+
+    print(cipher_text)
+    return cipher_text
+
+ciphered_text = encoder(message, shift)
+
+def decoder(ciphered_text, shift_amt):
+    cipher_text = ciphered_text
+    shift = shift_amt
+    original_text = ""
+    for letter in cipher_text:
+
+        if letter in alphabets:
+            newindex2 = (alphabets.index(letter) - shift) % 26 # here negative indexing works
+
+            original_text += alphabets[newindex2]
+
+        else:
+            original_text += letter
+
+    return original_text
+
+original = decoder(ciphered_text= ciphered_text, shift_amt=shift)
+print(original)
