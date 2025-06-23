ec4e1e133af9bf0825fedcd6831a80bb2010cbc5
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2025-06-23 22:48:58 +0530

    black jack game making

--- day 11/main.py
+++ day 11/main.py
@@ -1,66 +1,85 @@
 from art import *
-print(blackjack_art)
-
 import random
 
+print(blackjack_art)
+
 print("welcome to the blackjack game")
-
-
-
 cards = {
-    "red_heart" : [1,2,3,4,5,6,7,8,9,10,10,10,10],
-    "club" : [1,2,3,4,5,6,7,8,9,10,10,10,10],
-    "spade" : [1,2,3,4,5,6,7,8,9,10,10,10,10],
-    "diamonds" : [1,2,3,4,5,6,7,8,9,10,10,10,10]
-}
-keys = list(cards.keys())
+        "heart": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
+        "club": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
+        "spade": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10],
+        "diamonds": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    }
 
+def card_picking_dealer(cards):
+    # Make a list of suits that still have cards left
+    available_suits = [suit for suit in cards if len(cards[suit]) > 0]
+
+    # If no cards left, end the game
+    if len(available_suits) < 2:
+        print("No more cards! Game over.")
+
+    taken_out_suit_dealer = random.choice(available_suits)
+    dealer_card = random.choice(cards[taken_out_suit_dealer])
+
+    # Remove these cards from deck
+    cards[taken_out_suit_dealer].remove(dealer_card)
+
+    return taken_out_suit_dealer, dealer_card
+
+
+def card_picking_player(cards):
+
+    # Make a list of suits that still have cards left
+    available_suits = [suit for suit in cards if len(cards[suit]) > 0]
+
+    # If no cards left, end the game
+    if len(available_suits) < 2:
+        print("No more cards! Game over.")
+
+    taken_out_suit_player = random.choice(available_suits)
+    player_card = random.choice(cards[taken_out_suit_player])
+
+    # remove cards that are already picked
+    cards[taken_out_suit_player].remove(player_card)
+
+    return taken_out_suit_player, player_card
 
 
 dealer = 0
 player = 0
-taken_out_suits = {}
-
-
 
 game_over = False
 
 while not game_over:
-    # Make a list of suits that still have cards left
-    available_suits = [suit for suit in cards if len(cards[suit]) > 0]
 
-    # If no cards left, end game
-    if len(available_suits) < 2:
-        print("No more cards! Game over.")
-        break
-
-    taken_out_suit_dealer = random.choice(available_suits)
-    taken_out_suit_player = random.choice(available_suits)
-
-    dealer_card = random.choice(cards[taken_out_suit_dealer])
-    player_card = random.choice(cards[taken_out_suit_player])
-
-    # Remove these cards from deck
-    cards[taken_out_suit_dealer].remove(dealer_card)
-    cards[taken_out_suit_player].remove(player_card)
+    taken_out_suit_player, player_card = card_picking_player(cards)
+    taken_out_suit_dealer, dealer_card = card_picking_dealer(cards)
 
     if taken_out_suit_player != taken_out_suit_dealer and dealer_card != player_card:
 
-        print("player",taken_out_suit_player)
+        if player_card == 1 and player + player_card == 21 or player <= 10 :
+            player_card = 11
+
+        elif dealer_card == 1 and dealer + dealer_card == 21 or dealer <= 10 :
+            dealer_card = 11
+
+        print("player",taken_out_suit_player," player card ",player_card)
         player += player_card
+
         print(player ,"\n")
 
-        print("dealer", taken_out_suit_dealer)
+        print("dealer", taken_out_suit_dealer, " dealer card ",dealer_card)
         dealer += dealer_card
-        print(dealer,"\n")
 
+        print(dealer,"\n")
 
     if dealer >= 21 :
         print("player wins!")
         game_over = True
+
     elif player >= 21 :
         print("dealer wins!")
         game_over = True
-
 
 
